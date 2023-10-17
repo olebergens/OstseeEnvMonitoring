61982b91086df3c61a1d72b5a27278db0ea81138
Ole <37940650+olebergens@users.noreply.github.com>
2023-10-17 12:30:50 +0200

    added hoovering over datapoints

--- src/main/kotlin/views/AverageDataValuesView.kt
+++ src/main/kotlin/views/AverageDataValuesView.kt
@@ -3,8 +3,10 @@ package views
 import data.DataBaseHandler
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.geometry.Pos
+import javafx.scene.control.Label
 import tornadofx.*
 import java.time.LocalDate
+import java.time.LocalTime
 import kotlin.math.sqrt
 
 class AverageDataValuesView : View("Average Values") {
@@ -15,11 +17,33 @@ class AverageDataValuesView : View("Average Values") {
     private val toDatepicker = datepicker { value = LocalDate.now() }
     private val showFilteredData = SimpleBooleanProperty(false)
 
-
+    private val tempVal = Label("Value: ")
+    private val humidityVal = Label("Value: ")
+    private val pressureVal = Label("Value: ")
+    private val oxygenVal = Label("Value: ")
+    private val salinityVal = Label("Value: ")
+    private val turbidityVal = Label("Value: ")
+    private val waterTempVal = Label("Value: ")
     init {
-       // fromDatepicker.valueProperty().addListener { _, _, _ -> if (showFilteredData.get()) update() }
-        //toDatepicker.valueProperty().addListener { _, _, _ -> if (showFilteredData.get()) update() }
+        showFilteredData.addListener { _, _, newValue ->
+            if (newValue) {
+                updateFilteredData()
+            } else {
+                updateAllData()
+            }
+        }
 
+        fromDatepicker.valueProperty().addListener { _, _, _ ->
+            if (showFilteredData.get()) {
+                updateFilteredData()
+            }
+        }
+
+        toDatepicker.valueProperty().addListener { _, _, _ ->
+            if (showFilteredData.get()) {
+                updateFilteredData()
+            }
+        }
     }
 
     override val root = vbox {
@@ -36,62 +60,98 @@ class AverageDataValuesView : View("Average Values") {
             add(fromDatepicker)
             label("To Date:")
             add(toDatepicker)
+            checkbox("Filter", showFilteredData)
         }
         vbox(30.0) {
             padding = insets(50.0, 0.0, 0.0, 5.0)
             alignment = Pos.CENTER_LEFT
-            hbox(50.0){
+            hbox(50.0) {
                 label("Temperature")
-
+                add(tempVal)
             }
             hbox(50.0) {
                 label("Humidity")
-                label("Timestamp: ")
-                label("Value: ")
+                add(humidityVal)
             }
             hbox(50.0) {
                 label("Pressure")
-                label("Timestamp: ")
-                label("Value: ")
+                add(pressureVal)
             }
             hbox(50.0) {
                 label("Oxygen Content")
-                label("Timestamp: ")
-                label("Value: ")
+                add(oxygenVal)
             }
             hbox(50.0) {
                 label("Salinity")
-                label("Timestamp: ")
-                label("Value: ")
+                add(salinityVal)
             }
             hbox(50.0) {
                 label("Turbidity")
-                label("Timestamp: ")
-                label("Value: ")
+                add(turbidityVal)
             }
             hbox(50.0) {
                 label("Water Temperature")
-                label("Timestamp: ")
-                label("Value: ")
+                add(waterTempVal)
             }
         }
     }
 
-    private fun List<Double>.averageN(): Double {
-        return if (isEmpty()) {
+    private fun List<Int?>.averageN(): Double {
+        val nonNullValues = filterNotNull()
+        return if (nonNullValues.isEmpty()) {
             0.0
         } else {
-            sum() / size
+            nonNullValues.sum().toDouble() / nonNullValues.size
         }
     }
 
-    private fun List<Int>.standardDeviation(): Double {
-        val average = average()
-        val variance = if (size <=1) {
+    private fun List<Int?>.standardDeviation(): Double {
+        val nonNullValues = filterNotNull()
+        val average = nonNullValues.averageN()
+        val variance = if (nonNullValues.size <= 1) {
             0.0
         } else {
-            sumOf { (it - average) * (it - average) } / (size - 1)
+            nonNullValues.sumOf { (it - average) * (it - average) } / (nonNullValues.size - 1)
         }
         return sqrt(variance)
+    }
+
+    private fun updateFilteredData() {
+        val filteredData = sensorDataList.filter {
+            it.timestamp in fromDatepicker.value.atStartOfDay()..toDatepicker.value.atTime(
+                LocalTime.MAX)
+        }
+        val temperatureValues = filteredData.map { it.temperature }
+        val humidityValues = filteredData.map { it.humidity }
+        val pressureValues = filteredData.map { it.pressure }
+        val oxygenContentValues = filteredData.map { it.oxygenContent }
+        val salinityValues = filteredData.map { it.salinity }
+        val turbidityValues = filteredData.map { it.turbidity }
+        val waterTemperatureValues = filteredData.map { it.waterTemperature }
+
+        tempVal.text = "Average: ${temperatureValues.averageN()}"
+        humidityVal.text = "Average: ${humidityValues.averageN()}"
+        pressureVal.text = "Average: ${pressureValues.averageN()}"
+        oxygenVal.text = "Average: ${oxygenContentValues.averageN()}"
+        salinityVal.text = "Average: ${salinityValues.averageN()}"
+        turbidityVal.text = "Average: ${turbidityValues.averageN()}"
+        waterTempVal.text = "Average: ${waterTemperatureValues.averageN()}"
+    }
+
+    private fun updateAllData() {
+        val temperatureValues = sensorDataList.map { it.temperature }
+        val humidityValues = sensorDataList.map { it.humidity }
+        val pressureValues = sensorDataList.map { it.pressure }
+        val oxygenContentValues = sensorDataList.map { it.oxygenContent }
+        val salinityValues = sensorDataList.map { it.salinity }
+        val turbidityValues = sensorDataList.map { it.turbidity }
+        val waterTemperatureValues = sensorDataList.map { it.waterTemperature }
+        tempVal.text = "Average: ${temperatureValues.averageN()}"
+        humidityVal.text = "Average: ${humidityValues.averageN()}"
+        pressureVal.text = "Average: ${pressureValues.averageN()}"
+        oxygenVal.text = "Average: ${oxygenContentValues.averageN()}"
+        salinityVal.text = "Average: ${salinityValues.averageN()}"
+        turbidityVal.text = "Average: ${turbidityValues.averageN()}"
+        waterTempVal.text = "Average: ${waterTemperatureValues.averageN()}"
     }
 }
